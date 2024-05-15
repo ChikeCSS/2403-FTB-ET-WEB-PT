45add7f929e35904a18b4fbf6ac2cc04245ffaf8
ChikeCSS <165223693+ChikeCSS@users.noreply.github.com>
2024-05-15 18:56:26 -0500

    Add files via upload

--- Coursework/unit_2/block20/demo/index.js
+++ Coursework/unit_2/block20/demo/index.js
@@ -7,6 +7,13 @@ const state = {
  * 👉 STEP 1: Grab the `form`, `output` and div with a class of `total`
  * from the html document and save them to variables
  */
+const form = document.querySelector("form");
+const output = document.querySelector("output");
+const total = document.querySelector(".total");
+
+console.log("form", form);
+console.log("output", output);
+console.log("total", total);
 
 /**
  * 👉 STEP 2: Add an event listener to the form element which will wait for
@@ -14,6 +21,7 @@ const state = {
  *
  * Listening for `submit` is almost always better than the button `click`
  */
+form.addEventListener("submit", addNumber);
 
 /**
  * 👉 STEP 3: Add an event listener to the total element which will wait for
@@ -22,6 +30,7 @@ const state = {
  * This function uses event bubbling:
  *      we attach the event listener to the parent so that we don't have to attach it to each child
  */
+total.addEventListener("click", onNumberClick);
 
 /**
  * 👉 STEP 4: Complete the function so that it renders an array of numbers separated by commas to the element passed
@@ -32,7 +41,11 @@ const state = {
  * @param {Number[]} numberBank - array of integers
  * @param {Object} element - DOM element to replace the children of
  */
-function renderNumbers(numberBank, element) {}
+function renderNumbers(numberBank, element) {
+  ///[1, 2, 3] --> "1, 2, 3"
+  const commaNumberBank = numberBank.join(", ");
+  element.replaceChildren(commaNumberBank);
+}
 
 /**
  * 👉 STEP 5: Complete the function so that it adds the submitted number from the form into `numbers`, clears the form, and re-renders the page.
@@ -43,7 +56,32 @@ function renderNumbers(numberBank, element) {}
  *
  * @param {Event} event the deepest element that was clicked
  */
-function addNumber(event) {}
+function addNumber(event) {
+  //prevent page from refreshing
+  event.preventDefault();
+
+  //get the number from the form
+  const number = form.elements.number.value;
+
+  //check if it's actually a number
+  console.log(parseInt(number));
+  if (parseInt(number)) {
+    //add number to our state
+    state.numbers.push(number);
+    console.log(state.numbers);
+
+    //add number to running total
+    state.total += parseInt(number);
+
+    //re-render total on page
+    total.replaceChildren(state.total);
+
+    //re-render on numbers on page
+    renderNumbers(state.numbers, output);
+  }
+  //clear form
+  form.elements.number.value = null;
+}
 
 /**
  * 👉 STEP 6: Complete the function so that it updates the class of the event target to `active`
@@ -53,4 +91,11 @@ function addNumber(event) {}
  *
  * @param {Event} event the deepest element that was clicked
  */
-function onNumberClick(event) {}
+function onNumberClick(event) {
+  console.log(event.target.classList.contains("active"));
+  if (event.target.classList.contains("active")) {
+    event.target.classList.remove("active");
+  } else {
+    event.target.classList.add("active");
+  }
+}
